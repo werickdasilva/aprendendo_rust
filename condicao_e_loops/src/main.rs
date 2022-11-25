d4a35ecf86bad88a24df1f5927bca63eb7eee209
Werick da Silva <werick.s.santana34@gmail.com>
2022-11-25 07:06:51 -0300

    Explorando mais usos para o match

--- condicao_e_loops/src/main.rs
+++ condicao_e_loops/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 fn main() {
     let is_correndo = true;
     let is_andando= true;
@@ -35,12 +37,52 @@ fn main() {
         println!("Numeros é {numero}")
     }
 
-    let value_number = 10;
+    println!("=========== Usando o match expressão ===============");
+
+    let value_number = 5;
     match value_number {
         1 => println!("Numero 01"),
         2..=10 => println!("Numero entre 2 a 10"),
         12 | 14 | 16 => println!("Numero foi 12, 16, ou 18"),
-        _ => println!("Maior de idade")
+        _ => println!("Numero maior que 16")
     }
 
+    //  @ seria como uma renomeação de valores 
+    match value_number {
+        n @ 5 => println!("numero escolhido foi {n}"),
+        n @ 10 => println!("numero escolhido foi {n}"),
+        _ => println!("")
+    }
+
+
+    println!("match com Option .........................");
+    let some = Some(value_number);
+    match some {
+        Some(n) => println!("Tem valor e seu valor é {n}"),
+        None => println!("Não tem valor, então seu valor está sendo None")
+    }
+
+    println!("match com Result...............");
+    let result = File::open("./Cargo.toml");
+    match result {
+        Ok(_) => println!("Arquivo existe...."),
+        Err(_) => println!("Arquivo não existe............")
+    }
+
+    println!("match e usando if juntos");
+    match result {
+        Ok(file) => {
+            println!("Tem arquivo");
+            match file.metadata() {
+                Ok(_) => println!("Sem Informação sobre os dados do arquivo"),
+                
+                Ok(data) if data.is_file()  => {
+                    println!("è um arquivo ")
+                },
+                 Err(_) => println!("Deu Erro buscar informação sobre o arquivo")
+
+            }
+        },
+        Err(_) => println!("Error")
+    }
 }
